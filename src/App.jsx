54c3e7d61d0c596d20e616bbfc14aaf199b49218
unknown <diloch111@gmail.com>
2025-04-21 15:45:55 +0100

    create react project and loading screen component

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import LoadingScreen from './components/LoadingScreen'
+import './App.css'
+import "./index.css"
+import { useState, useEffect } from 'react'
+
+function App() {
+
+
+
+  return (
+    <>
+     <LoadingScreen />
+    </>
+  )
+}
+
+export default App
