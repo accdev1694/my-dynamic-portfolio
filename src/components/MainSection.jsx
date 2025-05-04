75994e79970cb9c3508f6884ed98a0d61ebcc404
unknown <diloch111@gmail.com>
2025-05-05 00:20:12 +0100

    complete about page responsive design

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -4,7 +4,7 @@ import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
 
 const MainSection = () => {
-  return <div className="px-14 z-60">
+  return <div className="px-14 lg:px-24 xl:px-42 z-60">
     <About />
     <Skills />
     <Projects />
--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -3,12 +3,11 @@ import HeadingOne from "../HeadingOne";
 import ImageCard from "../ImageCard";
 import NextPageBtn from "../NextPageBtn";
 
-
 const About = () => {
   return (
     <section
       id="about-me"
-      className="relative min-h-screen  flex flex-col-reverse lg:flex-row items-center justify-center lg:justify-between gap-4 lg:gap-0"
+      className="relative min-h-screen flex flex-col-reverse gap-8 items-center justify-center lg:justify-between lg:flex-row lg:pt-12"
     >
       <div className="lg:w-[60%] justfy-center text-center lg:text-right items-center">
         <h3 className="text-xs">
@@ -22,19 +21,23 @@ const About = () => {
           </span>{" "}
           Developer
         </h2>
-        <p className="leading-4 text-gray-500 mt-2 text-xs tracking-[1px]">
+        <p className="leading-6 text-gray-500 mt-2 text-sm tracking-[1px]">
           I started out in architecture, drawn to design and structure — but
           when I discovered web development, everything clicked. Coding quickly
           became more than a skill; it became the space where I feel most
           focused, creative, and at home. I can’t imagine doing anything else,
           and that passion drives everything I build.
         </p>
-        <div className="flex flex-col mt-8 gap-4">          
-          <NextPageBtn addClass="w-[100%]" text=">>> Checkout My Skills" next="#my-skills"/>
-          <ContactBtn addClass="w-[100%]"/>
+        <div className="flex flex-col sm:flex-row mt-12 gap-4">
+          <NextPageBtn
+            addClass="w-[100%]"
+            text=">>> Checkout My Skills"
+            next="#my-skills"
+          />
+          <ContactBtn addClass="w-[100%]" />
         </div>
       </div>
-      <div className="bubble w-[50%] md:w-[40%] bg-gradient-to-r from-[var(--color-accent-dark)] to-[var(--color-accent)] rounded-full">
+      <div className="bubble w-[40%] md:w-[30%] bg-gradient-to-r from-[var(--color-accent-dark)] to-[var(--color-accent)] rounded-full">
         <ImageCard
           url={"../../src/assets/images/my-photo.png"}
           classNames=""
